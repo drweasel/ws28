045f4ba89b5f2eac0fa498a7e78ea6c62736768f
Michael Weitzel <mich@elweitzel.de>
2025-07-27 12:46:10 +0200

    Reformatting with clang-format based on .clang-format file

--- src/Headers.h
+++ src/Headers.h
@@ -7,45 +7,56 @@
 #include <string_view>
 #include <optional>
 
-namespace ws28 {
-	class Client;
-	class Server;
-	
-	class RequestHeaders {
-	public:
-		void Set(std::string_view key, std::string_view value){
-			m_Headers.push_back({ key, value });
-		}
-		
-		template<typename F>
-		void ForEachValueOf(std::string_view key, const F &f) const {
-			for(auto &p : m_Headers){
-				if(p.first == key) f(p.second);
-			}
-		}
-		
-		std::optional<std::string_view> Get(std::string_view key) const {
-			for(auto &p : m_Headers){
-				if(p.first == key) return p.second;
-			}
-			
-			return std::nullopt;
-		}
-		
-		template<typename F>
-		void ForEach(const F &f) const {
-			for(auto &p : m_Headers){
-				f(p.first, p.second);
-			}
-		}
-		
-	private:
-		std::vector<std::pair<std::string_view, std::string_view>> m_Headers;
-		
-		friend class Client;
-		friend class Server;
-	};
-	
-}
+namespace ws28
+{
+class Client;
+class Server;
+
+class RequestHeaders
+{
+public:
+    void Set(std::string_view key, std::string_view value)
+    {
+        m_Headers.push_back({ key, value });
+    }
+
+    template< typename F >
+    void ForEachValueOf(std::string_view key, const F &f) const
+    {
+        for (auto &p : m_Headers)
+        {
+            if (p.first == key)
+                f(p.second);
+        }
+    }
+
+    std::optional< std::string_view > Get(std::string_view key) const
+    {
+        for (auto &p : m_Headers)
+        {
+            if (p.first == key)
+                return p.second;
+        }
+
+        return std::nullopt;
+    }
+
+    template< typename F >
+    void ForEach(const F &f) const
+    {
+        for (auto &p : m_Headers)
+        {
+            f(p.first, p.second);
+        }
+    }
+
+private:
+    std::vector< std::pair< std::string_view, std::string_view > > m_Headers;
+
+    friend class Client;
+    friend class Server;
+};
+
+} // namespace ws28
 
 #endif
